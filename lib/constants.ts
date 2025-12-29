8a4a9faeb3f7f75d0c10adbb325f79716c237b21
Joey <jguarino97@gmail.com>
2025-12-29 06:02:16 -0500

    Backup full app before public deploy

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
-import { generateDummyPassword } from "./db/utils";
 
 export const isProductionEnvironment = process.env.NODE_ENV === "production";
 export const isDevelopmentEnvironment = process.env.NODE_ENV === "development";
@@ -9,5 +8,3 @@ export const isTestEnvironment = Boolean(
 );
 
 export const guestRegex = /^guest-\d+$/;
-
-export const DUMMY_PASSWORD = generateDummyPassword();
