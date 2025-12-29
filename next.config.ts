e6bb078125916607b38c5b499cac5f43529b3e29
Joey <jguarino97@gmail.com>
2025-12-29 13:23:04 -0500

    Remove reactCompiler config entirely

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: false,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
